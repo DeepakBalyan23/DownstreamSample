00bae256bdfe335dd7844bbb0cdfbf537c0ffe4c
DeepakBalyan23 <deepakbalyan23@gmail.com>
2018-08-07 11:21:30 +0530

    getter_setter

--- src/main/java/com/stackroute/eplay/downstreamservice/domain/Event.java
+++ src/main/java/com/stackroute/eplay/downstreamservice/domain/Event.java
@@ -14,4 +14,33 @@ public class Event {
    /* public Date eventStartTime;
     public Date eventEndTime;*/
     public String eventCity;
+    
+    public Event() {}
+    
+	public Event(String eventId, String eventName, String eventCity) {
+	super();
+	this.eventId = eventId;
+	this.eventName = eventName;
+	this.eventCity = eventCity;
+}
+	public String getEventId() {
+		return eventId;
+	}
+	public void setEventId(String eventId) {
+		this.eventId = eventId;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public String getEventCity() {
+		return eventCity;
+	}
+	public void setEventCity(String eventCity) {
+		this.eventCity = eventCity;
+	}
+    
+    
 }
